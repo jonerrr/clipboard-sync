f02d181b206a6a0bebc23039547c1483a85ed93a
Jonah <73760377+jonerrr@users.noreply.github.com>
2023-12-16 20:10:16 -0500

    add image sync and work on tls

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
-use arboard::Clipboard;
+use arboard::{Clipboard, ImageData};
 use clap::{Parser, Subcommand};
 use errors::ProgramError;
 use futures_channel::mpsc::{unbounded, UnboundedSender};
 use futures_util::{future, pin_mut, stream::TryStreamExt, StreamExt};
+use local_ip_address::local_ip;
+// use native_tls::TlsConnector;
 use std::{
     collections::HashMap,
     net::SocketAddr,
@@ -35,6 +37,9 @@ enum Commands {
         /// Address to connect to
         #[arg(short, long, default_value = "127.0.0.1")]
         address: String,
+        /// Use TLS (NOT IMPLEMENTED YET)
+        #[arg(short, long, default_value = "false")]
+        tls: bool,
     },
     /// Starts sync server
     Server {
@@ -47,23 +52,28 @@ enum Commands {
     },
 }
 
-#[derive(Subcommand)]
-enum ClientCommands {
-    /// Starts sync client
-    Start {
-        /// Port to listen on
-        #[arg(short, long, default_value = "4343")]
-        port: u16,
-        /// Address to listen on
-        #[arg(short, long, default_value = "127.0.0.1")]
-        address: String,
-    },
+#[derive(PartialEq, Clone)]
+struct Image {
+    bytes: Vec<u8>,
+    width: usize,
+    height: usize,
+}
+
+// convert Image to Vec<u8> and include the width and hieght in the vec
+impl Into<Vec<u8>> for Image {
+    fn into(self) -> Vec<u8> {
+        let mut vec = Vec::new();
+        vec.extend_from_slice(&self.width.to_be_bytes());
+        vec.extend_from_slice(&self.height.to_be_bytes());
+        vec.extend_from_slice(&self.bytes);
+        vec
+    }
 }
 
 #[derive(PartialEq)]
 enum ClipboardData {
     Text(String),
-    Image(Vec<u8>),
+    Image(Image),
 }
 
 #[tokio::main]
@@ -72,13 +82,21 @@ async fn main() -> Result<(), ProgramError> {
     let cli = Cli::parse();
 
     match &cli.command {
-        Commands::Client { port, address } => {
-            let addr = format!("ws://{}:{}", address, port);
+        Commands::Client { port, address, tls } => {
             let (clipboard_tx, clipboard_rx) = futures_channel::mpsc::unbounded();
             tokio::spawn(read_clipboard(clipboard_tx));
 
+            let protocol: &str = if *tls { "wss" } else { "ws" };
+            // let connector = if *tls {
+            //     let mut builder = TlsConnector::builder();
+            //     builder.danger_accept_invalid_certs(true);
+            //     Connector::NativeTls(builder.build().unwrap())
+            // } else {
+            //     Connector::Plain
+            // };
+            let addr = format!("{}://{}:{}", protocol, address, port);
             let (ws_stream, _) = tokio_tungstenite::connect_async(&addr).await?;
-            tracing::info!("Connected to: {}", addr);
+            tracing::info!("Websocket connection established with: {}", addr);
 
             let (write, read) = ws_stream.split();
             let clipboard_to_ws = clipboard_rx.map(Ok).forward(write);
@@ -90,8 +108,20 @@ async fn main() -> Result<(), ProgramError> {
 
                         let _ = match message.unwrap() {
                             Message::Text(text) => clipboard.set_text(text),
-                            Message::Binary(bin) => {
-                                clipboard.set_image(todo!("convert to image data"))
+                            Message::Binary(img) => {
+                                let width = usize::from_be_bytes(img[0..8].try_into().unwrap());
+                                let height = usize::from_be_bytes(img[8..16].try_into().unwrap());
+                                let bytes = img[16..].to_vec();
+                                let img = Image {
+                                    bytes,
+                                    width,
+                                    height,
+                                };
+                                clipboard.set_image(ImageData {
+                                    bytes: img.bytes.into(),
+                                    width: img.width,
+                                    height: img.height,
+                                })
                             }
                             _ => Ok(()),
                         };
@@ -106,7 +136,8 @@ async fn main() -> Result<(), ProgramError> {
         Commands::Server { port, address } => {
             let addr = format!("{}:{}", address, port);
             let listener = TcpListener::bind(&addr).await?;
-            tracing::info!("Listening on: {}", addr);
+            let local_ip = local_ip().unwrap();
+            tracing::info!("Listening on: {}\nLocal IP: {}", addr, local_ip);
             let peers = PeerMap::new(Mutex::new(HashMap::new()));
 
             while let Ok((stream, addr)) = listener.accept().await {
@@ -123,32 +154,16 @@ async fn main() -> Result<(), ProgramError> {
     Ok(())
 }
 
-async fn read_clipboard(tx: futures_channel::mpsc::UnboundedSender<Message>) {
-    let mut clipboard = Clipboard::new().unwrap();
-    let mut last_clipboard: ClipboardData = ClipboardData::Text("".to_owned());
-
-    loop {
-        if let Ok(data) = clipboard.get_text() {
-            if last_clipboard != ClipboardData::Text(data.clone()) {
-                last_clipboard = ClipboardData::Text(data.clone());
-                tracing::info!("clipboard: {:#?}", data);
-                tx.unbounded_send(Message::text(data)).unwrap();
-            }
-        }
-        tokio::time::sleep(tokio::time::Duration::from_millis(1000)).await;
-    }
-}
-
 type Tx = UnboundedSender<Message>;
 type PeerMap = Arc<Mutex<HashMap<SocketAddr, Tx>>>;
 
 async fn handle_connection(raw_stream: TcpStream, addr: SocketAddr, peer_map: PeerMap) {
-    println!("Incoming TCP connection from: {:?}", addr);
+    // tracing::info!("Incoming TCP connection from: {:?}", addr);
 
     let ws_stream = tokio_tungstenite::accept_async(raw_stream)
         .await
         .expect("Error during the websocket handshake occurred");
-    println!("WebSocket connection established: {:?}", addr);
+    tracing::info!("Connected to: {:?}", addr);
 
     // Insert the write part of this peer to the peer map.
     let (tx, rx) = unbounded();
@@ -157,11 +172,11 @@ async fn handle_connection(raw_stream: TcpStream, addr: SocketAddr, peer_map: Pe
     let (outgoing, incoming) = ws_stream.split();
 
     let broadcast_incoming = incoming.try_for_each(|msg| {
-        println!(
-            "Received a message from {:?}: {}",
-            addr,
-            msg.to_text().unwrap()
-        );
+        // println!(
+        //     "Received a message from {:?}: {}",
+        //     addr,
+        //     msg.to_text().unwrap()
+        // );
         let peers: std::sync::MutexGuard<'_, HashMap<SocketAddr, UnboundedSender<Message>>> =
             peer_map.lock().unwrap();
 
@@ -183,6 +198,38 @@ async fn handle_connection(raw_stream: TcpStream, addr: SocketAddr, peer_map: Pe
     pin_mut!(broadcast_incoming, receive_from_others);
     future::select(broadcast_incoming, receive_from_others).await;
 
-    println!("{:?} disconnected", &addr);
+    tracing::info!("{:?} disconnected", &addr);
     peer_map.lock().unwrap().remove(&addr);
+}
+
+async fn read_clipboard(tx: futures_channel::mpsc::UnboundedSender<Message>) {
+    let mut clipboard = Clipboard::new().unwrap();
+    let mut last_clipboard: ClipboardData = ClipboardData::Text("".to_owned());
+
+    //TODO: use less clones
+    loop {
+        if let Ok(data) = clipboard.get_text() {
+            if last_clipboard != ClipboardData::Text(data.clone()) {
+                last_clipboard = ClipboardData::Text(data.clone());
+                // tracing::info!("clipboard: {:#?}", data);
+                tx.unbounded_send(Message::text(data)).unwrap();
+            }
+        } else {
+            if let Ok(data) = clipboard.get_image() {
+                let img = Image {
+                    bytes: data.bytes.to_vec(),
+                    width: data.width,
+                    height: data.height,
+                };
+
+                if last_clipboard != ClipboardData::Image(img.clone()) {
+                    last_clipboard = ClipboardData::Image(img.clone());
+                    tracing::info!("clipboard img: {:#?}", data.width);
+                    //TODO impl into vec<u8> for Image
+                    tx.unbounded_send(Message::binary(img)).unwrap();
+                }
+            }
+        }
+        tokio::time::sleep(tokio::time::Duration::from_millis(1000)).await;
+    }
 }
